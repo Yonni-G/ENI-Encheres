18373201ed0a8245c3a4f30d184f814be4231d3b
antoinecoulon <a.antoinecoulon@gmail.com>
2024-12-16 15:59:50 +0100

    Renommer 'EnchereController'

--- src/main/java/fr/eni/eniencheres/eniencheres/controller/EnchereController.java
+++ src/main/java/fr/eni/eniencheres/eniencheres/controller/EnchereController.java
@@ -1,13 +1,17 @@
 package fr.eni.eniencheres.eniencheres.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class EnchereController {
 
     @GetMapping({"/", "/encheres"})
-    public String accueil() {
+    public String accueil(Model model) {
+
+
+
         return "pages/encheres/encheres";
     }
 
