f6e9268bb1e0f95b47394f9a42277b6949d1967b
Yonni-G <yonni4@gmail.com>
2024-12-16 16:13:42 +0100

    page inscription

--- src/main/java/fr/eni/eniencheres/eniencheres/controller/UtilisateurController.java
+++ src/main/java/fr/eni/eniencheres/eniencheres/controller/UtilisateurController.java
@@ -1,13 +1,27 @@
 package fr.eni.eniencheres.eniencheres.controller;
 
+import fr.eni.eniencheres.eniencheres.bo.Utilisateur;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UtilisateurController {
 
     @GetMapping("/inscription")
-    public String UtilisateurInscriptionGet(){
+    public String UtilisateurInscriptionGet(Model model) {
+
+        model.addAttribute("utilisateur", new Utilisateur());
+        return "pages/utilisateur/inscription";
+    }
+
+    @GetMapping("/inscription")
+    public String UtilisateurInscriptionPost(@Valid Utilisateur utilisateur, BindingResult controUser, Model model) {
+        if(controUser.hasErrors()) {
+
+        }
         return "pages/utilisateur/inscription";
     }
 }
