9b3d33613b16a9ed97701d417ca3ee1dd2adee8a
antoinecoulon <a.antoinecoulon@gmail.com>
2024-12-16 14:13:57 +0100

    "Créer controller 'Home'"

---
+++ src/main/java/fr/eni/eniencheres/eniencheres/controller/HomeController.java
@@ -0,0 +1,14 @@
+package fr.eni.eniencheres.eniencheres.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", "/encheres"})
+    public String accueil() {
+        return "encheres"; //TODO: créer vue 'encheres'
+    }
+
+}
