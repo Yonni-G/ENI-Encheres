1bef0372a49902c5d60e65aabc9f0e7545bf639d
antoinecoulon <a.antoinecoulon@gmail.com>
2024-12-16 16:17:43 +0100

    Ajouter model Categories

--- src/main/java/fr/eni/eniencheres/eniencheres/controller/EnchereController.java
+++ src/main/java/fr/eni/eniencheres/eniencheres/controller/EnchereController.java
@@ -1,5 +1,7 @@
 package fr.eni.eniencheres.eniencheres.controller;
 
+import fr.eni.eniencheres.eniencheres.dal.EnchereRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class EnchereController {
 
+    @Autowired
+    private EnchereRepository repository;
+
     @GetMapping({"/", "/encheres"})
     public String accueil(Model model) {
 
-
+        model.addAttribute("categories", repository.findAllCategories());
 
         return "pages/encheres/encheres";
     }
