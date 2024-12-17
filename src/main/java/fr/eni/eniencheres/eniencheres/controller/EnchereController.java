0dc3d3b4f47e79bce8a102537c189d9cb4a5f723
antoinecoulon <a.antoinecoulon@gmail.com>
2024-12-17 15:26:20 +0100

    Ajouter mapping vers detailVente.html

--- src/main/java/fr/eni/eniencheres/eniencheres/controller/EnchereController.java
+++ src/main/java/fr/eni/eniencheres/eniencheres/controller/EnchereController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -46,4 +47,8 @@ public class EnchereController {
         return "pages/encheres/encheres";
     }
 
+    @GetMapping("/detailVente/{noArticle}")
+    public String detailVente(@PathVariable("noArticle") Integer noArticle, Model model) {
+        return "pages/encheres/detailVente";
+    }
 }
