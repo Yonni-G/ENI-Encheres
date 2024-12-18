0546eb766c614dd58bded17638d5ae657c70eb03
Yonni-G <yonni4@gmail.com>
2024-12-18 11:00:14 +0100

    gestion inscription

---
+++ src/main/java/fr/eni/eniencheres/eniencheres/security/AuthenticationService.java
@@ -0,0 +1,22 @@
+package fr.eni.eniencheres.eniencheres.security;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationService(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    public Authentication authenticate(String pseudo, String motDePasse) {
+        Authentication authentication = new UsernamePasswordAuthenticationToken(pseudo, motDePasse);
+        return authenticationManager.authenticate(authentication);
+    }
+}
+
