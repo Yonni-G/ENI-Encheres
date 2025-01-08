51d035d4bdf8ec87fd5ff2044a2191e22e9e71f4
antoinecoulon <a.antoinecoulon@gmail.com>
2025-01-08 12:06:50 +0100

    Ajouter une image à la vente d'un article

--- src/main/java/fr/eni/eniencheres/eniencheres/controller/ArticleController.java
+++ src/main/java/fr/eni/eniencheres/eniencheres/controller/ArticleController.java
@@ -2,12 +2,14 @@ package fr.eni.eniencheres.eniencheres.controller;
 
 import fr.eni.eniencheres.eniencheres.bll.ArticleVenduService;
 import fr.eni.eniencheres.eniencheres.bll.EnchereService;
+import fr.eni.eniencheres.eniencheres.bll.FileStorageService;
 import fr.eni.eniencheres.eniencheres.bll.UtilisateurService;
 import fr.eni.eniencheres.eniencheres.bo.ArticleVendu;
 import fr.eni.eniencheres.eniencheres.bo.Retrait;
 import fr.eni.eniencheres.eniencheres.bo.Utilisateur;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,7 +17,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Optional;
 
 
@@ -26,6 +31,8 @@ public class ArticleController {
     private EnchereService enchereService;
     private UtilisateurService utilisateurService;
     private ArticleVenduService articleVenduService;
+    @Autowired
+    private FileStorageService fileStorageService;
 
     ArticleController(EnchereService enchereService, UtilisateurService utilisateurService, ArticleVenduService articleVenduService) {
         this.enchereService = enchereService;
@@ -62,7 +69,7 @@ public class ArticleController {
     }
 
     @PostMapping("/vendre")
-    public String vendrePost(@Valid ArticleVendu articleVendu, BindingResult controleArticleVendu, Model model){
+    public String vendrePost(@Valid ArticleVendu articleVendu, BindingResult controleArticleVendu, MultipartFile file, Model model){
 
         // il faut également injecter les catégories
         model.addAttribute("categories", enchereService.findAllCategories());
@@ -84,6 +91,21 @@ public class ArticleController {
         }
         else {
             model.addAttribute("errorMessage", "Impossible de trouver cet utilisateur");
+            return "pages/articles/formAjoutArticle";
+        }
+
+        // On ajoute l'image (upload)
+        try {
+            // Sauvegarde de l'image et récupérer chemin du fichier
+            String filePath = fileStorageService.saveFile(file);
+            System.out.println("Chemin de l'image sauvegardée : " + filePath);
+
+            // On affecte à l'objet ArticleVendu
+            articleVendu.setLien_image(filePath);
+            model.addAttribute("message", "Image uploadé avec succès!");
+        } catch (IOException e) {
+            e.printStackTrace();
+            model.addAttribute("message", "Impossible de sauver le fichier"  + e.getMessage());
             return "pages/articles/formAjoutArticle";
         }
 
